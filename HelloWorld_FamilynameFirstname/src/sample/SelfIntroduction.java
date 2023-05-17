c9b21646084f79db10185c36d23647314e98d35a
Chie <Chie@192.168.0.11>
2023-05-17 21:38:05 +0900

    1_3 課題新規提出

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,26 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		
+		// 氏名
+		String name = "山田 花子";
+		// 年齢
+		int age = 25;
+		// 趣味
+		String hobby = "映画鑑賞";
+
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+
+		// 私の名前は山田 花子です。
+		System.out.println("私の名前は" + name + "です。");
+		// 年齢は25歳です。
+		System.out.println("年齢は" + age + "歳です。");
+		// 趣味は映画観賞です。
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+}
